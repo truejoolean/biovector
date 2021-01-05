d754993abb7ddf7ec5c88a64e4632e8c3004bb78
JayMaster <kontakt@julienjaacks.de>
2021-01-05 10:25:50 +0100

    more on the findTalent page

--- pages/findTalent.js
+++ pages/findTalent.js
@@ -26,33 +26,70 @@ export default function FindTalent() {
 			</div>
 		</section>
 		<section className="mt-8 w-screen-lg">
-			<h2 className="text-4xl text-center font-semibold">We are unlike any other job platform.</h2>
+			<h2 className="text-4xl text-center font-semibold">We are unlike any other job platform. Here's why:</h2>
 			<div className="flex mx-auto max-w-screen-lg mt-8">
 				<div className="w-1/2 border-r-2">
-					<div className="flex flex-col align-items w-96 items-center mx-auto">
-						<div className="w-20 h-20 bg-gray-400" />
-						<h2 className="text-xl">Targeted listings</h2>
-						<p className="text-center">Your listings drown on other platforms in a sea of 1000s of listings. The Biovector helps them stay afloat.</p>
+					<div className="flex flex-col align-items w-96 items-center mx-auto h-56 hover:bg-gray-300">
+						<img className="w-24" src="/images/icons/icon_target.png" />
+						<h2 className="text-xl mt-2">Targeted listings</h2>
+						<p className="text-center mt-2">Other platforms drown your expensive listing in a sea of 1000s of job postings. The Biovector's selectivity and filtering helps them stay afloat.</p>
 					</div>
-					<div className="flex flex-col align-items w-96 items-center mx-auto mt-4 pt-8 border-t-2">
-						<div className="w-20 h-20 bg-gray-400" />
-						<h2 className="text-xl">Targeted listings</h2>
-						<p className="text-center">Your listings drown on other platforms in a sea of 1000s of listings. The Biovector helps them stay afloat.</p>
+					<hr className="mt-2"/>
+					<div className="flex flex-col align-items w-96 items-center mx-auto mt-4 h-56">
+						<img className="w-24" src="/images/icons/icon_pricing.png" />
+						<h2 className="text-xl mt-2">Fair pricing</h2>
+						<p className="text-center mt-2">We want you to invest into amazing talents, not recruiters.</p>
 					</div>
 				</div>
 				<div className="w-1/2">
-					<div className="flex flex-col align-items w-96 items-center mx-auto">
-						<div className="w-20 h-20 bg-gray-400" />
-						<h2 className="text-xl">Targeted listings</h2>
-						<p className="text-center">Your listings drown on other platforms in a sea of 1000s of listings. The Biovector helps them stay afloat.</p>
+					<div className="flex flex-col align-items w-96 items-center mx-auto h-56">
+						<img className="w-24" src="/images/icons/icon_scientist.png" />
+						<h2 className="text-xl mt-2">Focussed expertise</h2>
+						<p className="text-center mt-2">Having experience in multiple biotechnology enterprises, we know the intricacies of this special labor market.</p>
 					</div>
-					<div className="flex flex-col align-items w-96 items-center mx-auto mt-4 pt-8 border-t-2">
-						<div className="w-20 h-20 bg-gray-400" />
-						<h2 className="text-xl">Targeted listings</h2>
-						<p className="text-center">Your listings drown on other platforms in a sea of 1000s of listings. The Biovector helps them stay afloat.</p>
+					<hr className="mt-2" />
+					<div className="flex flex-col align-items w-96 items-center mx-auto mt-4 h-56">
+						<img className="w-24" src="/images/icons/icon_global.png" />
+						<h2 className="text-xl mt-2">International talents</h2>
+						<p className="text-center mt-2">We attract high-potentials from around the world to Germany</p>
 					</div>
 				</div>
 			</div>
+		</section>
+		<section className="mt-16 max-w-screen-lg mx-auto">
+			<h2 className="text-4xl text-center font-semibold">Features & Pricing</h2>
+			<table className="mt-4 w-full">
+				<tr>
+					<th className="w-3/12"></th>
+					<th className="w-4/12 bg-blue-900 text-white text-3xl font-normal border p-4">Basic</th>
+					<th className="w-4/12 bg-blue-900 text-white text-3xl font-normal border p-4">Pro</th>
+				</tr>
+				<tr>
+					<td className="w-3/12 bg-blue-900 text-white text-xl p-4 text-center font-light">Price</td>
+					<td className="border w-4/12 text-center text-2xl">350<span className="text-xs font-normal">€</span></td>
+					<td className="border w-4/12 text-center text-2xl">750<span className="text-xs font-normal">€</span></td>
+				</tr>
+				<tr>
+					<td className="w-3/12 bg-blue-900 text-white text-xl p-4 text-center font-light border">Months of exposure</td>
+					<td className="border w-4/12 text-center font-bold text-2xl">3</td>
+					<td className="border w-4/12 text-center font-bold text-2xl">6</td>
+				</tr>
+				<tr>
+					<td className="w-3/12 bg-blue-900 text-white text-xl p-4 text-center font-light border">Investment into targeted advertisement</td>
+					<td className="border w-4/12 text-center font-bold text-2xl">70<span className="text-xs font-normal">€</span></td>
+					<td className="border w-4/12 text-center font-bold text-2xl">120<span className="text-xs font-normal">€</span></td>
+				</tr>
+				<tr>
+					<td className="w-3/12 bg-blue-900 text-white text-xl p-4 text-center font-light border">Extra post on Google Jobs</td>
+					<td className="border w-4/12 text-center font-bold text-2xl"><img className="mx-auto" src="/images/icons/icon_tick.svg" /></td>
+					<td className="border w-4/12 text-center font-bold text-2xl"><img className="mx-auto" src="/images/icons/icon_tick.svg" /></td>
+				</tr>
+				<tr>
+					<td className="w-3/12 bg-blue-900 text-white text-xl p-4 text-center font-light border">Manual management of advertisement campaigns</td>
+					<td className="border w-4/12 text-center font-bold text-2xl"><img className="mx-auto" src="/images/icons/icon_tick.svg" /></td>
+					<td className="border w-4/12 text-center font-bold text-2xl"><img className="mx-auto" src="/images/icons/icon_cross.svg" /></td>
+				</tr>
+			</table>
 		</section>
 
 		
