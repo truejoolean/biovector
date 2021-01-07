5be5e7f1b40d09fda0aee7a04750b7e234bca356
JayMaster <kontakt@julienjaacks.de>
2021-01-07 09:39:17 +0100

    Update navBar.js

--- components/navBar.js
+++ components/navBar.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-export default function NavBar() {
+/* export default function NavBar() {
 	return (
 		<nav className="h-16 w-full bg-white flex items-center justify-between">
 			<Link href="/"><a>
@@ -7,5 +7,11 @@ export default function NavBar() {
 			</a></Link>
 			<Link href="/findTalent"><a className="no-underline"><div className="mr-16 bg-blue-700 text-white rounded-lg border-2 px-4 py-2">I want to list</div></a></Link>
 		</nav>
+	)
+} */
+
+export default function NavBar() {
+	return(
+		<div />
 	)
 }
