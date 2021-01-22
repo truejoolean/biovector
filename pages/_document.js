d29e838bb32f404af90e6385f2057e02d6541770
JayMaster <kontakt@julienjaacks.de>
2021-01-22 13:05:59 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -34,7 +34,7 @@ class MyDocument extends Document {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-XXXXXX');`}} />
+          })(window,document,'script','dataLayer','GTM-55MR6K4');`}} />
         </Head>
         <body>
           <Main />
