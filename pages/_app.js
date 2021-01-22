656ce558dcd3363257667d8fd66b1493c9f0955b
JayMaster <kontakt@julienjaacks.de>
2021-01-22 11:36:53 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -34,22 +34,11 @@ const MyApp = ({ Component, pageProps }) => {
     <>
       <Head>
         {/* <link rel="shortcut icon" href={getStrapiMedia(global.favicon)} /> */}
-      {/*!-- Global site tag (gtag.js) - Google Analytics -->*/}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q61QL9P521"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-Q61QL9P521');
-        </script>
-       {/*}<!-- Google Tag Manager -->*/}
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-55MR6K4');</script>
-{/*<!-- End Google Tag Manager -->*/}
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-55MR6K4');</script>
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Staatliches"
@@ -67,10 +56,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
-    {/*<!-- Google Tag Manager (noscript) -->*/}
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-55MR6K4"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-{/*<!-- End Google Tag Manager (noscript) -->*/}
       <GlobalContext.Provider value={{global: global, activeFilters: activeFilters}}>
         <Component {...pageProps} />
       </GlobalContext.Provider>
