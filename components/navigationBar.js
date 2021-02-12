66f687ac3fdd12c2a0dd6d6d2776390a866cb58b
JayMaster <kontakt@julienjaacks.de>
2021-02-12 09:19:13 +0100

    navbar opacity

--- components/navigationBar.js
+++ components/navigationBar.js
@@ -2,11 +2,11 @@ import Link from 'next/link'
 
 export default function NavigationBar() {
 	return (
-		<nav className="h-16 w-full bg-white flex items-center justify-between">
+		<nav className="absolute h-16 w-full flex items-center justify-between">
 			<Link href="/"><a>
 				<div className="ml-4"><img src="/images/logo_blue.svg" className="h-8"/></div>
 			</a></Link>
-			<Link href="/findTalent"><a className="no-underline"><div className="mr-16 md:mr-4 bg-blue-700 text-white rounded-lg border-2 px-4 py-2 md:py-2 md:text-xs">I want to list</div></a></Link>
+			<Link href="/findTalent"><a className="no-underline"><div className="mr-16 md:mr-4 bg-blue-700 text-white rounded-lg px-4 py-2 md:py-2 md:text-xs">I want to list</div></a></Link>
 		</nav>
 	)
 }
