ba6a937603db4dc4e10e8046450b09bb33e0ecef
JayMaster <kontakt@julienjaacks.de>
2021-03-17 15:06:07 +0100

    cookie update

--- pages/_document.js
+++ pages/_document.js
@@ -17,7 +17,7 @@ class MyDocument extends Document {
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
           />
-          <script
+          <script type="text/plain" data-cookieconsent="analytics"
             // eslint-disable-next-line react/no-danger
             dangerouslySetInnerHTML={{
               __html: `
@@ -30,7 +30,7 @@ class MyDocument extends Document {
               `,
             }}
           />
-          <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          <script type="text/plain" data-cookieconsent="analytics" dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
