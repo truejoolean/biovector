dff5a6ef2357db98289d830bb2155952873f0099
JayMaster <kontakt@julienjaacks.de>
2021-03-19 15:15:49 +0100

    fix spacing for sitemap gen

--- util/sitemap-generator.js
+++ util/sitemap-generator.js
@@ -45,9 +45,8 @@ async function generateSiteMap() {
     '!pages/modal.js'
   ])
 
-  const sitemap = `
-      <?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
           ${pages
             .map(page => {
               const path = page
