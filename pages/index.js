cd2804e79a8b55dfd7e4d7782cd7b7757e16a7bc
JayMaster <kontakt@julienjaacks.de>
2021-04-12 18:59:45 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -89,7 +89,6 @@ export default function Home({ listings, allCities, allStates }) {
   return (
 	<Layout bg="bg-gray-100" footer={true} navbarAbsolute={true}>
 		<Head>
-			<script id="mcjs" dangerouslySetInnerHTML={{__html: `!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/ac98e47188302a54db3dff986/2fa6448b43f36c8da429d1fbe.js");`}} />
 			<link rel="alternate" hreflang="en" href="https://biovector.de/"/>
 			<link rel="alternate" hreflang="de" href="https://biovector.de/de"/>
 			<link rel="alternate" hreflang="x-default" href="https://biovector.de/"/>
@@ -97,7 +96,7 @@ export default function Home({ listings, allCities, allStates }) {
 			<title>{translate("sitetitle", lang)}</title>
 			<meta name="description" content={translate("pageMetaTitle", lang)} />
 		</Head>
-
+		<script id="mcjs" dangerouslySetInnerHTML={{__html: `!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/ac98e47188302a54db3dff986/2fa6448b43f36c8da429d1fbe.js");`}} />
 		<div className="bannerHomePage">
 			<div className="mx-auto max-screen-lg md:w-11/12 py-32">
 				<h1 className="text-5xl md:text-3xl font-semibold inline-block p-2" style={{ background: 'rgba(255,255,255,.9)' }}>{translate("tagline", lang)}</h1>
